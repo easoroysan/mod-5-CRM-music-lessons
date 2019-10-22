dbf412779645e77684135b9260cbd15573f0373e
Roysan Easo <easo.roysan@gmail.com>
2019-10-22 15:38:25 -0500

    still figuring out cors stuff

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://musiclessonsorganizer.com/'
+    origins 'musiclessonsorganizer.com'
 
     resource '*',
       headers: :any,
